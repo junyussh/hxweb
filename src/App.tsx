b44383f2b64b3f3877c018cbe2d7b588814e12ce
Chun Yu <junyussh@gmail.com>
2022-08-20 20:29:35 +0800

    feat: flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Tree from './Tree'
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
-import {getSmile, Node, NodeType} from './getData';
+import { getSmile, Node, NodeType } from './getData';
+import { Navbar, Button, TextInput } from 'flowbite-react'
 import useForceUpdate from './useForceUpdate';
 // import LineChart from './d3Tree'
 
@@ -21,10 +22,10 @@ function App() {
     //   type: NodeType.Root,
     //   smile: smile
     // }
-    setTreeData({...treeData, smile: smile})
+    setTreeData({ ...treeData, smile: smile })
     // treeData.smile = smile
     console.log(treeData);
-    
+
     const smiles: Node[] = getSmile(treeData)
     console.log(smiles);
   }
@@ -32,89 +33,76 @@ function App() {
     smile: "#",
     level: -1,
     type: NodeType.Root,
-    // children: [
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "BrCc1ccccc1.CC1(C)[C@H]2CN[C@H](CO)[C@H]21"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21.O=Cc1ccccc1"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "BrCc1ccccc1.CC1(C)[C@H]2CN[C@H](CO)[C@H]21.Cl"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21.Cl"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21.ClCc1ccccc1"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21.Cl.ClCc1ccccc1"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "BrCc1ccccc1.CC1(C)C2CNC(CO)C21"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC1(C)[C@H]2CN[C@H](CO)[C@H]21.Cl.O=Cc1ccccc1"
-    //   },
-    //   {
-    //     level: 0,
-    //     type: NodeType.Node,
-    //     smile: "CC(C)(C)[Si](C)(C)OC[C@@H]1NC[C@H]2[C@@H]1C2(C)C.O=Cc1ccccc1"
-    //   }
-    // ]
   });
   return (
-    <div className="App" style={{height: "100vh"}}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <>
+      <Navbar
+        fluid={false}
+        rounded={false}
+        className="max-w-7xl py-2"
+      >
+        <Navbar.Brand href="https://flowbite.com/">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link
+            href="/navbars"
+            active={true}
+          >
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">
+            About
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">
+            Services
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">
+            Pricing
+          </Navbar.Link>
+          <Navbar.Link href="/navbars">
+            Contact
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="container" style={{ height: "100vh" }}>
+        {/* <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div> */}
+        <h1>Vite + React</h1>
+        <div className="card">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
+        <TextInput type="text" value={smile} id="" onChange={e => setSmile(e.target.value)} />
+        <Button onClick={handleCalculate}>Go</Button>
+        {treeData.children?.map(e => <p key={e.smile}>{e.smile}</p>)
+        }
+        <ParentSize>{({ width, height }) => <Tree width={width} height={height} data={treeData} setData={setTreeData} />}</ParentSize>,
+        {/* <LineChart></LineChart> */}
+        {/* <BasicTree data={ data } /> */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <input type="text" value={smile} id="" onChange={e => setSmile(e.target.value)} />
-      <button onClick={handleCalculate}>Go</button>
-      {treeData.children?.map(e => <p key={e.smile}>{e.smile}</p>)
-      }
-      <ParentSize>{({ width, height }) => <Tree width={width} height={height} data={treeData} setData={setTreeData} />}</ParentSize>,
-      {/* <LineChart></LineChart> */}
-      {/* <BasicTree data={ data } /> */}
-    </div>
+    </>
   )
 }
 
